9eab985a5669b147b55c2e6f337735e523ef58a7
TrevorOregbomGitHub <105277543+TrevorOregbomGitHub@users.noreply.github.com>
2022-06-29 15:10:08 +0100

    Array list commit

---
+++ ArrayListTest.java
@@ -0,0 +1,20 @@
+package com.qa.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+	
+	public static void main(String[] args){
+	
+	List <String> country = new ArrayList<>(); 
+	
+	country.add("Argentina");
+	country.add("Belgium");
+	country.add("Italy");
+	country.add("Italy");
+	
+	System.out.println(country);
+
+	}
+}
