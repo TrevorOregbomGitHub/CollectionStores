41e1d1e8567f6cb8a5201ac57374d47f6d7ed21c
TrevorOregbomGitHub <105277543+TrevorOregbomGitHub@users.noreply.github.com>
2022-06-29 15:08:47 +0100

    Hash set commit

---
+++ HashSetTest.java
@@ -0,0 +1,18 @@
+package com.qa.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+	
+	public static void main(String[] args) {
+	
+	HashSet<String> set = new HashSet<String>();
+	set.add("RW");
+	set.add("CM");
+	set.add("CB");
+	set.add("CB");
+	
+	System.out.println(set);
+
+	}
+}
